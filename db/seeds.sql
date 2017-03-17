f5a8fdd7c86f238dee82cf75b0c6930bd6e7f84e
tom c <tom c>
2017-03-16 23:48:17 -0400

    working but ugly

---
+++ db/seeds.sql
@@ -0,0 +1,9 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Sound & The Curry Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Band On The Bun Burger - Comes with Wings.", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Final Kraut Down Burger (Comes with sauerkraut)", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Bruschetta Bout It Burger", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Fifth Day of Christmas Burger - (Comes with five golden rings of onion)", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Only the Provolonely Burger - (Comes with provolone)
+", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("The Cauliflower's Cumin from Inside the House Burger  - (Comes with cauliflower and cumin)", false);
+INSERT INTO burgers (burger_name, devoured) VALUES ("Sweet Home Avocado Burger", false);
---
+++ server.js
@@ -0,0 +1,34 @@
+// ==============================================================================
+// DEPENDENCIES
+// Series of npm packages that we will use to give our server useful functionality
+// ==============================================================================
+
+var express = require("express");
+var bodyParser = require("body-parser");
+var methodOverride = require("method-override");
+
+var PORT = process.env.PORT || 3000
+
+
+var app = express();
+
+// Serve static content for the app from the "public" directory in the application directory.
+app.use(express.static(process.cwd() + "/public"));
+
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// Override with POST having ?_method=DELETE
+app.use(methodOverride("_method"));
+
+// Set Handlebars.
+var exphbs = require("express-handlebars");
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+// Import routes and give the server access to them.
+var routes = require("./controllers/burgers_Controller.js");
+
+app.use("/", routes);
+
+app.listen(PORT);
