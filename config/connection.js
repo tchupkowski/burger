7d3cd4db092d67089a8761ff31d47d98fc5bac62
tom c <tom c>
2017-03-18 18:29:23 -0400

    for heroku 2

--- config/connection.js
+++ config/connection.js
@@ -5,8 +5,7 @@ var connection;
 if (process.env.JAWSDB_URL){
   connection = mysql.createConnection(process.env.JAWSDB_URL);
 } else {
-    connection = mysql.createConnection({
-    port: 3306,
+    connection = mysql.createConnection({    
     host: "localhost",
     user: "root",
     password: "moments1",
@@ -18,13 +17,7 @@ if (process.env.JAWSDB_URL){
 
 
 // Make connection.
-connection.connect(function(err) {
-  if (err) {
-    console.error("error connecting: " + err.stack);
-    return;
-  }
-  console.log("connected as id " + connection.threadId);
-});
+connection.connect();
 
 // Export connection for our ORM to use.
 module.exports = connection;
